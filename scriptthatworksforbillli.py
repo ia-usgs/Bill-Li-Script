c272ad8d94b22777f904ea04394176235ba5f2b3
iavitiausgs <97401201+iavitiausgs@users.noreply.github.com>
2022-06-13 15:20:13 -0700

    Update scriptthatworksforbillli.py

--- scriptthatworksforbillli.py
+++ scriptthatworksforbillli.py
@@ -6,56 +6,69 @@ Created on Thu Jun  9 12:50:02 2022
 
 This script was created for Bill Li.
 The purpose of this script is to show last date, Voltage, and location of certain 
-sites with different parameters.
+sites with different parameters. It will now also display Ground Water and Surface Water parameters.
 """
 # first import the functions for downloading data from NWIS
 
 import dataretrieval.nwis as nwis
 
-#List of all the sites to be checked (code is not ready yet but need to link to bottom code)
+#INPUT PROPER SITES HERE:
 
-allSites = ["323313117033901", "323313117033902", "323313117033903"]
+surfacesite = '11042000'
+groundsite = '323932117050801'
 
-all_sites = []
+#SURFACE WATER CODE
 
-for i in allSites:
-    all_sites.append(i)
-    
+site = surfacesite
 
+df = nwis.get_record(sites=site, service='measurements')
 
+#GROUND WATER CODE
 
-#print(all_sites)
+site2 = groundsite
 
-# specify the USGS site code for which we want data.
+#df4 must have it's parameters checked against what keys are availabe
 
-site = '323932117050801'
-# get instantaneous values (iv)
-
-#df = nwis.get_record(sites=site, service='iv', start='2017-12-31', end='2018-01-01')
-# get water quality samples (qwdata)
-
-#df2 = nwis.get_record(sites=site, service='qwdata', start='2017-12-31', end='2018-01-01')
-# get basic info about the site
-
-df8 = nwis.get_record(sites=site,service='iv', start='2021-12-01', access='3', parameterCd='70969')
-
-#this code will iterate through all the voltages that are 11 or above and also displays the site they belong to
-#the code is left as a comment in order to keep it clean. Remove hashtag to run it
-
-#for k in df8["70969"]:
-#    if k >= 11:
-#        print("This is the voltage: {} this is the site: {}".format(k, site))
-
-#This code grabs the records, organizes them by the parameterCd and displays them based on the site
-df4 = nwis.get_record(sites=site, service='iv', start='2017-12-31', parameterCd='70969')
-
+df4 = nwis.get_record(sites=site2, service='iv', start='2017-12-31', parameterCd='70969')
 df5 = df4.iloc[-1]
 
-#This code iterates through all the voltage and site number, then displays the last line which shows latest information
+#Program iterates through voltage, then appends them to df6
 df6 = []
 
 for i in df4["70969"]:
     df6.append(i)
+  
+#Program iterates through all the site numbers, then appends them to df7 
+df7 = []
+
+for j in df4["site_no"]:
+    df7.append(j)
+
+
+#PRINTING ALL INFORMATION
+print('********************************************************************')
+print("SURFACE WATER SITE INFORMATION")
+print(site, df.measurement_dt.iat[-1])
+print('********************************************************************')
+#The following code is just to organize the data into something legible
+print("THE LAST VOLTAGE FOR SITE # {} IS: {}".format(df7[-1], df6[-1]))
+
+#print("BELOW IS THE BASIC SITE INFORMATION")
+print("___________________________________________________________________")
+print("GROUND WATER LAST LOCATION WITH SITE NUMBER, DATE, AND VOLTAGE")
+print("********************************************************************")
+print(df5)
+
+print("********************************************************************")
+print("IF ANY DATA IS DISPLAYED BELOW THIS STATEMENT, IT IS BECAUSE VOLTAGE IS BELOW 11")
+print("********************************************************************")
+
+#Program iterates through all voltage, then displays anything which is 11 or below.
+for k in df4["70969"]:
+    if k <= 11:
+        df = pd.DataFrame([k, j], ['Voltage', 'Site No.'])
+        print(df)
+print("********************************************************************")
   
 
 df7 = []
